126667456acdd7b4e705e2ec36b1d9238fdc4473
Youssef-codin <youssef240100538@sut.edu.eg>
2025-12-11 22:37:52 +0200

    implemented 2 searches (BST and binary search) and 1 sorting algo (Bubblesort)

---
+++ search/search.cpp
@@ -0,0 +1,32 @@
+void bubbleSort(int array[], int size){
+    int temp;
+
+    for(int i = 0; i < size - 1; i++) {
+        for(int j = 0; j < size - i - 1; j++) {
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                j = 0;
+            }
+        }
+    }
+}
+
+int binarySearch(int arr[], int size, int val) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;  
+
+        if (arr[mid] == val)
+            return mid;           
+        else if (arr[mid] < val)
+            left = mid + 1;       
+        else
+            right = mid - 1;     
+    }
+
+    return -1;
+}
