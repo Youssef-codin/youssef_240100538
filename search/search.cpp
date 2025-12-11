dc3ced5a035695365998bb92bce9996e58ad5787
Youssef-codin <youssef240100538@sut.edu.eg>
2025-12-11 22:39:03 +0200

    fixed search

--- search/search.cpp
+++ search/search.cpp
@@ -18,14 +18,14 @@ int binarySearch(int arr[], int size, int val) {
     int right = size - 1;
 
     while (left <= right) {
-        int mid = left + (right - left) / 2;  
+        int mid = (right - left) / 2;
 
         if (arr[mid] == val)
-            return mid;           
+            return mid;
         else if (arr[mid] < val)
-            left = mid + 1;       
+            left = mid + 1;
         else
-            right = mid - 1;     
+            right = mid - 1;
     }
 
     return -1;
